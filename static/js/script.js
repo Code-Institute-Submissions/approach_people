e597dac9f8f216135cf2cf40916acad4b46a5609
todorr92 <todorovicslobodan10@gmail.com>
2020-09-07 11:42:55 +0000

    added comments to script.js and style.css

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
    $("#posted_date").datepicker({ 
     });
     // Getting todays date for Date Posted input field
+    // source: https://stackoverflow.com/questions/233553/how-do-i-pre-populate-a-jquery-datepicker-textbox-with-todays-date#:~:text=You%20must%20FIRST%20call%20datepicker,to%20get%20the%20current%20date.&text=var%20myDate%20%3D%20new%20Date()%3B,%2B%20'%2F'%20%2B%20myDate.
     var myDate = new Date();
     var month = myDate.getMonth() + 1;
     var prettyDate = myDate.getDate() + '/' + month + '/' + myDate.getFullYear();

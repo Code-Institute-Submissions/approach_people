d443afe94ae3ea6eb705f60ff5d4fcdd5c756ded
todorr92 <todorovicslobodan10@gmail.com>
2020-09-11 13:05:01 +0000

    disabled possibility to choose before and after todays date in posted date input field

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
   $(".sidenav").sidenav();
   $("select").formSelect();
+  // Disable selecting any date other then todays one for a input field posted date
+  var dateToday = new Date(); 
    $("#posted_date").datepicker({ 
+        minDate: dateToday,
+        maxDate: dateToday
     });
-    // Getting todays date for Date Posted input field
-    // source: https://stackoverflow.com/questions/233553/how-do-i-pre-populate-a-jquery-datepicker-textbox-with-todays-date#:~:text=You%20must%20FIRST%20call%20datepicker,to%20get%20the%20current%20date.&text=var%20myDate%20%3D%20new%20Date()%3B,%2B%20'%2F'%20%2B%20myDate.
-    var myDate = new Date();
-    var month = myDate.getMonth() + 1;
-    var prettyDate = myDate.getDate() + '/' + month + '/' + myDate.getFullYear();
-    $("#posted_date").val(prettyDate);
 });
 $(".datepicker").datepicker({
   selectMonths: true, // Creates a dropdown to control month
@@ -23,3 +21,4 @@ $(".datepicker").datepicker({
 $('.close-modal').click(function() {
     location.reload();
 });
+

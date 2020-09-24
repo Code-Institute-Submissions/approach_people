cea679b1bb1fcb2b92cf745e882028ed3ca99249
todorr92 <todorovicslobodan10@gmail.com>
2020-09-24 11:36:50 +0000

    Formated and beatufied sendEmail.js

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,24 +1,25 @@
 // EMAIL JS
-(function(){
-      emailjs.init("user_NCVoKpfkDgC3bkJIJPFHw");
-   })();
-function sendMail(contactForm){
-    emailjs.send("gmail", "approach_people",{
-        "from_name":contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        // Showing modal if form valid
-        function(response){
-            $('.modal').modal('show');
-            $(".close-modal").click(function(){
-                location.reload();
+(function() {
+    emailjs.init("user_NCVoKpfkDgC3bkJIJPFHw");
+})();
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "approach_people", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "message": contactForm.message.value
+        })
+        .then(
+            // Showing modal if form valid
+            function(response) {
+                $('.modal').modal('show');
+                $(".close-modal").click(function() {
+                    location.reload();
+                });
+            },
+            // Alerting message below if form not valid
+            function(error) {
+                alert("Fail, please try again.");
             });
-        },
-        // Alerting message below if form not valid
-        function(error){
-             alert("Fail, please try again.");
-        });
     return false; // To block from loading a new page
 }
